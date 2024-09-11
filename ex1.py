32428752466e2e4781cb5da1704c4c6851c9fcbb
lidwahh <walidhoummady@gmail.com>
2024-09-11 16:25:16 -0400

    jai monstre

--- ex1.py
+++ ex1.py
@@ -1,5 +1,5 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est le nom de l'athlète ?")
+date = input("Quelle est la date de son accomplissement ?")
